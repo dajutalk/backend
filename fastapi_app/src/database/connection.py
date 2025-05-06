ee7a5e7b0381c2783dd318bf0f17e68de510b5cf
JunEon <junun24@gmail.com>
2025-05-06 18:09:36 +0900

    필요없는 기능 삭제

--- fastapi_app/src/database/connection.py
+++ fastapi_app/src/database/connection.py
@@ -1,12 +1,12 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from beanie import init_beanie, Document
-from ..models.stock import Event, EventUpdate
+from ..models.stock import Stock
 from config import settings
 
 async def initialize_database():
     try:
         client= AsyncIOMotorClient(settings.Database_URL)
-        await init_beanie(database=client.get_default_database(), document_models=[Event])
+        await init_beanie(database=client.get_default_database(), document_models=[Stock])
     except Exception as e:
         print(f"Error initializing database: {e}")
     
@@ -14,21 +14,7 @@ class Database:
     def __init__(self, model:Document) -> None:
         self.model = model
 
-    async def create(self, document: Document) :
-        await document.create()
-        return
-    
     async def get(self) -> list:
         docs = await self.model.find_all().to_list()
         return docs
     
-
-    async def update(self, id: int, body: EventUpdate):
-        doc = await self.model.get(id)
-        body=body.model_dump()
-        body = {"$set":{k: v for k, v in body.items() if v is not None}}
-        await doc.update(body)
-    
-    async def delete(self, id: int):
-        doc = await self.model.get(id)
-        await doc.delete()
