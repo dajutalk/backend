e89d80a16185059d57a15cb96ae28893a94ad496
JunEon <junun24@gmail.com>
2025-05-06 18:28:33 +0900

    pydantic 응답 모델

---
+++ fastapi_app/src/shcemas/stock.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+from typing import Optional, List, Dict
+
+class StockSchema(BaseModel):
+    ticker: str
+    companyName: Optional[str]
+    sector: Optional[str]
+    isin: Optional[str]
+    marketCap: Optional[float]
+    priceNow: Optional[float]
+    priceChangePercent: Optional[float]
+    volume: Optional[int]
+    per: Optional[float]
+    dividendYield: Optional[float]
+    chartData: Optional[List[Dict]] = []
+
+    class Config:
+        orm_mode = True
