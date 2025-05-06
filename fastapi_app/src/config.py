3c557e40312f30e31efd8547468ab5c5df684226
JunEon <junun24@gmail.com>
2025-05-06 18:23:09 +0900

    mysql 에 config 수정

--- fastapi_app/src/config.py
+++ fastapi_app/src/config.py
@@ -1,9 +1,7 @@
-from pydantic import ConfigDict
 from pydantic_settings import BaseSettings
 
-
 class Settings(BaseSettings):
-    Database_URL: str
+    DATABASE_URL: str
 
     class Config:
         env_file = ".env"
