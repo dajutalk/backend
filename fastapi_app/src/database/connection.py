ed6f7321c4d526ae94045ad78faf3e3bcadaf5e3
JunEon <junun24@gmail.com>
2025-05-06 18:24:50 +0900

    orm 방식으로 수정

--- fastapi_app/src/database/connection.py
+++ fastapi_app/src/database/connection.py
@@ -1,12 +1,12 @@
-# src/database/connection.py
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker, declarative_base
 from ..config import settings
 
 DATABASE_URL = settings.DATABASE_URL
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+Base = declarative_base()
 
 async def get_db():
     async with AsyncSessionLocal() as session:
