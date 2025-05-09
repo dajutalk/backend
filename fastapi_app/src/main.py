d4a528564c73f0f4eb2370ad4303d6f86340a905
JunEon <junun24@gmail.com>
2025-05-09 11:03:52 +0900

    Revert "분당 주식 정보 받아오기"
    This reverts commit 61313cf90fd265abe5791f5becb319ddc7e3b227.

--- fastapi_app/src/main.py
+++ fastapi_app/src/main.py
@@ -2,23 +2,11 @@ import os
 from contextlib import asynccontextmanager
 from fastapi import FastAPI, WebSocket,WebSocketDisconnect
 from fastapi.responses import FileResponse
-from .database.connection import engine, Base
-from .routes import stock
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    # 앱 시작 시
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-    yield
-
-app = FastAPI(
-    lifespan=lifespan,
-)
 
 
 connections = []
-app.include_router(stock.router, prefix="/api")
+
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_PATH = os.path.join(BASE_DIR, "templates", "index.html")
