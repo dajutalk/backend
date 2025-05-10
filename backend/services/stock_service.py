f1690aee13737faf405136094cc6e84d251b5df6
JunEon <junun24@gmail.com>
2025-05-10 19:12:05 +0900

    키 설정

--- backend/services/stock_service.py
+++ backend/services/stock_service.py
@@ -2,7 +2,11 @@ import asyncio
 import websocket
 import json
 from backend.api.stock import broadcast_stock_data
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
+API_KEY = os.getenv("FINNHUB_API_KEY")
 loop = asyncio.get_event_loop()
 
 
@@ -21,7 +25,7 @@ def run_ws(loop):
         ws.send('{"type":"subscribe","symbol":"BINANCE:BTCUSDT"}')
 
     ws = websocket.WebSocketApp(
-        "wss://ws.finnhub.io?token=d0fgaf9r01qsv9ehav00d0fgaf9r01qsv9ehav0g",
+        f"wss://ws.finnhub.io?token={API_KEY}",
         on_message=on_message,
         on_open=on_open
     )
