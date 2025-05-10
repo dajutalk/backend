e79aa7d02055f91a891b48534bce9a2174f8fc00
JunEon <junun24@gmail.com>
2025-05-10 15:51:13 +0900

    client_id로 특정 보내기 없앰

--- backend/main.py
+++ backend/main.py
@@ -15,8 +15,6 @@ class ConnectionManager:
     def disconnect(self, websocket: WebSocket):
         self.active_connections.remove(websocket)
 
-    async def send_personal_message(self, message: str, websocket: WebSocket):
-        await websocket.send_text(message)
 
     async def broadcast(self, message: str):
         for connection in self.active_connections:
@@ -32,16 +30,15 @@ async def get():
     return FileResponse(TEMPLATE_PATH)
 
 
-@app.websocket("/ws/{client_id}")
-async def websocket_endpoint(websocket: WebSocket, client_id: int):
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
     await manager.connect(websocket)
     try:
         while True:
             data = await websocket.receive_text()
-            await manager.send_personal_message(f"You wrote: {data}", websocket)
-            await manager.broadcast(f"Client #{client_id} says: {data}")
+            await manager.broadcast(f"Client  says: {data}")
     except WebSocketDisconnect:
         manager.disconnect(websocket)
-        await manager.broadcast(f"Client #{client_id} left the chat")
+        await manager.broadcast(f"Client  left the chat")
 
         
