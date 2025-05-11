37e8d4911e4e016c9c137395b0258a5d5687855f
JunEon <junun24@gmail.com>
2025-05-11 15:27:08 +0900

    핑 주고 받기 삭제 및 prefix

--- backend/api/stock.py
+++ backend/api/stock.py
@@ -2,18 +2,18 @@ from fastapi import APIRouter, WebSocket, WebSocketDisconnect
 import asyncio
 import json
 
-router = APIRouter()
+router = APIRouter(
+    prefix="/ws",
+    tags=["WebSocket"],
+    responses={404: {"description": "Not found"}},
+)
 clients = []
 
-@router.websocket("/ws/stocks")
+@router.websocket("/stocks")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     clients.append(websocket)
-    try:
-        while True:
-            await asyncio.sleep(10)  # heartbeat 또는 ping 유지용
-            await websocket.send_text(json.dumps({"type": "ping"}))
-    except WebSocketDisconnect:
+    if  WebSocketDisconnect:
         clients.remove(websocket)
 
 # 외부 WebSocket(Finnhub) → 이 clients 리스트에 broadcast
@@ -25,5 +25,5 @@ async def broadcast_stock_data(data: dict):
             await ws.send_text(json.dumps(data))
             living.append(ws)
         except Exception as e:
-            print("❌ 전송 실패:", e)
+            print("전송 실패:", e)
     clients[:] = living
