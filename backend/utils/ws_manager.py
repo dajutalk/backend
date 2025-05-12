1173fec9cf62a0250c8a6e9590f2e0fb6be53473
JunEon <junun24@gmail.com>
2025-05-12 11:31:32 +0900

    해당 심볼을 선택한 클라이언트에게만 보내기

--- backend/utils/ws_manager.py
+++ backend/utils/ws_manager.py
@@ -7,19 +7,21 @@ clients_lock =Lock()
 
 async def safe_add_client(ws: WebSocket):
     async with clients_lock:
-        clients.append(ws)
+        clients.append({"websocket": ws, "symbol": symbol})
 
 async def safe_remove_client(ws: WebSocket):
     async with clients_lock:
-        clients.remove(ws)
+        clients[:] = [
+            client for client in clients if client["websocket"] != ws
+        ]
 
 async def broadcast_stock_data(data: dict):
-    print("브로드캐스트 시작:", data)  # 로그 추가
-    living = []
-    for ws in clients:
-        try:
-            await ws.send_text(json.dumps(data))
-            living.append(ws)
-        except Exception as e:
-            print("전송 실패:", e)
-    clients[:] = living
+    print("브로드캐스트 시작:", data) 
+    symbol = data["data"][0]["s"]
+    async with clients_lock:
+        for client in clients:
+            if client["symbol"] == symbol:
+                try:
+                    await client["websocket"].send_text(json.dumps(data))
+                except Exception as e:
+                    print("전송 실패:", e)
