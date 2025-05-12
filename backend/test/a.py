4101e42afd5ad63f5583245e2ae1916223273073
JunEon <junun24@gmail.com>
2025-05-12 16:23:28 +0900

    종목을 가상화폐로 변경

--- backend/test/a.py
+++ backend/test/a.py
@@ -1,7 +1,6 @@
 
 import finnhub
 
-API_KEY = "d0fgaf9r01qsv9ehav00d0fgaf9r01qsv9ehav0g"  # 또는 os.getenv("FINNHUB_API_KEY")
 
 finnhub_client = finnhub.Client(api_key="d0fgaf9r01qsv9ehav00d0fgaf9r01qsv9ehav0g")
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,14 +2,16 @@ import { useNavigate } from "react-router-dom";
 
 export default function Home() {
   const navigate = useNavigate();
-  const symbols = ["AAPL", "TSLA", "BINANCE:BTCUSDT"];
+  const symbols = ["BINANCE:ETHUSDT", "BINANCE:BNBUSDT", "BINANCE:BTCUSDT","BINANCE:SOLUSDT","BINANCE:XRPUSDT"];
 
 return (
     <div>
       <h1>종목 선택</h1>
-      <button onClick={() => navigate('/stocks/AAPL')}>AAPL</button>
-      <button onClick={() => navigate('/stocks/TSLA')}>TSLA</button>
-      <button onClick={() => navigate('/stocks/BINANCE:BTCUSDT')}>BTCUSDT</button>
+      <button onClick={() => navigate('/stocks/BINANCE:ETHUSDT')}>이더리움</button>
+      <button onClick={() => navigate('/stocks/BINANCE:BNBUSDT')}>바이낸스</button>
+      <button onClick={() => navigate('/stocks/BINANCE:BTCUSDT')}>비트코인</button>
+      <button onClick={() => navigate('/stocks/BINANCE:SOLUSDT')}>솔라나</button>
+      <button onClick={() => navigate('/stocks/BINANCE:XRPUSDT')}>리플</button>
     </div>
   );
 }
