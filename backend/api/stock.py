a4e4f5dde3434e0218a001adbac808f498400089
JunEon <junun24@gmail.com>
2025-05-12 13:26:03 +0900

    심볼을 쿼리로 받기

--- backend/api/stock.py
+++ backend/api/stock.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect, Query
 from backend.utils.ws_manager import safe_add_client,safe_remove_client
+import threading
 import asyncio
 import json
-
+from backend.services.stock_service import run_ws  
 router = APIRouter(
     prefix="/ws",
     tags=["WebSocket"],
@@ -11,9 +12,13 @@ router = APIRouter(
 
 
 @router.websocket("/stocks")
-async def websocket_endpoint(websocket: WebSocket, symbol: str):
+async def websocket_endpoint(websocket: WebSocket, symbol= Query(...)):
     await websocket.accept()
-    await safe_add_client(websocket, symbol)
+    await safe_add_client(websocket)
+
+    loop = asyncio.get_event_loop()
+    threading.Thread(target=run_ws, args=(loop, symbol), daemon=True).start()
+
     try:
         while True:
             await asyncio.sleep(10)
