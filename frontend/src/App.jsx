e8f46d3d619c47dd715e161d170caef79dd55608
JunEon <junun24@gmail.com>
2025-05-12 11:38:43 +0900

    Home 추가

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import StockPanel from './components/StockPanel'
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div>
-      
-      <StockPanel />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/stock/:symbol" element={<StockPanel />} />
+      </Routes>
+    </Router>
   );
 }
 
