13229a72f33c7ec0f359ed1367344ad2e20ed996
JunEon <junun24@gmail.com>
2025-05-13 22:59:31 +0900

    stock 안에 다 넣음

--- stock/backend/api/stock.py
+++ stock/backend/api/stock.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect, Query
-from backend.utils.ws_manager import safe_add_client,safe_remove_client
+from stock.backend.utils.ws_manager import safe_add_client,safe_remove_client
 import threading
 import asyncio
 import json
-from backend.services.stock_service import run_ws  
+from stock.backend.services.stock_service import run_ws  
 router = APIRouter(
     prefix="/ws",
     tags=["WebSocket"],
